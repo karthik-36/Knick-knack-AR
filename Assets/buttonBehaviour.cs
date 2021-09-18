f10965fd375cc10a8c96b97fc4ebb89252a4c4ab
karthik singh <31167294+karthik-36@users.noreply.github.com>
2021-09-18 17:40:10 -0500

    all done

---
+++ Assets/buttonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string sceneName){
+        Debug.Log("scene is hit  " + sceneName);
+       // Application.LoadLevel(sceneName);
+
+       SceneManager.LoadScene(sceneName);
+    }
+}
